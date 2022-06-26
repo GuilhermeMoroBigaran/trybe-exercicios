3c7aca7ad3566354ac641c3c2a28455c2174b226
GuilhermeMoroBigaran <gmbigaran@gmail.com>
2022-06-26 19:27:50 -0300

    modifica arquivo ex7weekDays

--- fundamentos/bloco4-intro-javascript-logica/dia1-intro-javascript/ex7WeekDay.js
+++ fundamentos/bloco4-intro-javascript-logica/dia1-intro-javascript/ex7WeekDay.js
@@ -1,13 +1,14 @@
 let weekDay = 'terca';
+console.log('Hoje é ' + weekDay);
 
 if (weekDay === 'sábado' || weekDay ==='domingo' ) {
-    console.log('dia de descanso!');
+    console.log('Dia de descansar! Aproveite bem!');
 }
 
-else if (weekDay === 'segunda' || weekDay === 'terça' || weekDay === 'quarta' || weekDay === 'quinta' || weekDay === 'sexta') {
+else if (weekDay === 'segunda' || weekDay === 'terça' || weekDay === 'quarta' || weekDay === 'quinta' || weekDay === 'sexta' || weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
     console.log('Oba, mais um dia de Trybe!');
 }
 
 else {
-    console.log('informação inválida! Por favor, digite um dia da semana')
+    console.log('Valor inválido! Por favor, digite um dia da semana. Utilize apenas letras minúsculas')
 }
