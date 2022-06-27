a5f1d2cf89834ed9ecc6d96c9f04b1649b0113a4
GuilhermeMoroBigaran <gmbigaran@gmail.com>
2022-06-27 13:50:02 -0300

    adiciona ex2maior

---
+++ fundamentos/bloco4-intro-javascript-logica/dia1-intro-javascript/ex2maior.js
@@ -0,0 +1,13 @@
+const a = 3;
+const b = 2;
+
+console.log("\na = " + a);
+console.log("b = " + b);
+
+if (a > b){
+    console.log("\nMaior número:\na = ", a);
+} else if (b > a){
+    console.log("\nMaior número:\nb = ", b);
+}else {
+    console.log("Os números (a) e (b) são iguais");
+}
